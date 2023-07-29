438f43059ed4172dfc03cbc8aa4d4aa734a09883
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2023-07-29 09:23:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Menu e barra de pesquisa responsivos
-let link_pagina_nav_li1 = document.querySelector("li.link-pagina-nav-li1");
-let link_pagina_nav_li2 = document.querySelector("li.link-pagina-nav-li2");
-let form_pesquisa = document.querySelector(".form-li");
+const link_pagina_nav_li1 = document.querySelector("li.link-pagina-nav-li1");
+const link_pagina_nav_li2 = document.querySelector("li.link-pagina-nav-li2");
+const form_pesquisa = document.querySelector(".form-li");
 document.querySelector("li.menu-responsivo-li").onclick = () => {
     link_pagina_nav_li1.classList.toggle("ativo");
     link_pagina_nav_li2.classList.toggle("ativo");
